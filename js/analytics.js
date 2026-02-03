da54b9c1f70a39d3f63407cd091da44c394955c1
ISROisTHEbest <taksheelshah2009@gmail.com>
2026-02-03 17:57:12 +0530

    V1.3.3 Added Analytics

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-01VR4RQ5HB"
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-01VR4RQ5HB');
